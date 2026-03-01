de2189e2fef449e205c52ec0d05e91d2dc98a3a7
lucas <lucasfalvesferreira@gmailcom>
2026-03-01 18:26:45 -0300

    Adição de atributos para a configuração de Spaw

--- core/src/main/kotlin/com/tcc/client/config/SpawnCfg.kt
+++ core/src/main/kotlin/com/tcc/client/config/SpawnCfg.kt
@@ -1,7 +1,16 @@
 package com.tcc.client.config
 
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType
+import ktx.math.vec2
+
+
 data class SpawnCfg(
     val atlasKey: String,
+    val bodyType: BodyType = BodyType.DynamicBody,
+    val scalePhysic: Vector2 = vec2(),
+    val physicOffset: Vector2 = vec2(),
+    val scaleSpeed: Float = 1f
 
 ){
     companion object{
